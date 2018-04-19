97f55cd143b86b85256e4a398b46842293696ec5
bwz3kt <bwz3kt@virginia.edu>
2018-04-19 16:33:38 -0400

    Added chrome drivers and uncommented for testing

--- selenium/selenium_script.py
+++ selenium/selenium_script.py
@@ -13,65 +13,65 @@ class SeleniumTests(unittest.TestCase):
             command_executor='http://selenium-chrome:4444/wd/hub',
             desired_capabilities=DesiredCapabilities.CHROME)
 
-    ## COMMENTED OUT b/c cannot pass through Travis.
-    # def test_user_login(self):
-    #     username = "cyeung"
-    #     password = "password"
-    #     driver = self.driver
-    #     driver.implicitly_wait(20)
-    #     #my ip address
-    #     #driver.get("http://192.168.99.100:8000/")
-    #
-    #     #Travis
-    #     driver.get("http://174.138.62.246:8000/")
-    #     driver.find_element_by_id("id_username").send_keys(username)
-    #     driver.find_element_by_id("id_password").send_keys(password)
-    #     driver.find_element_by_id("login").click()
-    #     driver.implicitly_wait(20)
-    #     #Log out shit doesn't work right now.
-    #     #logout_text = driver.find_element_by_id("logout").text
-    #     #driver.implicitly_wait(20)
-    #     #self.assertEqual("Log Out", logout_text)
-    #
-    # def test_user_signup(self):
-    #     username = "testuser"
-    #     email="test@virginia.edu"
-    #     password = "newpassword"
-    #     confirmpassword = "newpassword"
-    #     driver = self.driver
-    #     driver.implicitly_wait(20)
-    #     #my ip address
-    #     #driver.get("http://192.168.99.100:8000/signup/")
-    #
-    #     #travis
-    #     driver.get("http://174.138.62.246:8000/signup/")
-    #
-    #     driver.find_element_by_id("id_username").send_keys(username)
-    #     driver.find_element_by_id("id_email").send_keys(email)
-    #     driver.find_element_by_id("id_password").send_keys(password)
-    #     driver.find_element_by_id("id_passwordConfirm").send_keys(confirmpassword)
-    #     driver.find_element_by_id("signup").click()
-    #     driver.implicitly_wait(20)
-    #
-    #     #logout_text not working
-    #     #logout_text = driver.find_element_by_id("logout").text
-    #     #self.assertEqual("Log Out", logout_text)
-    #
-    #
-    #
-    #     #Just the test I made, but doesn't work -Taehyun-
-    #     #driver.find_element_by_xpath("/base/body/nav/div[2]/div[1]/div[1]/li/a").click()
-    #     #assert "Log Out:" in driver.page_source
-    #     #driver.implicitly_wait(20)
-    #
-    #
-    # def test_home_page(self):
-    #     driver = self.driver
-    #     #My ip address
-    #     #driver.get("http://192.168.99.100:8000/home/")
-    #
-    #     #Travis
-    #     driver.get("http://174.138.62.246:8000/home/")
+    # COMMENTED OUT b/c cannot pass through Travis.
+    def test_user_login(self):
+        username = "cyeung"
+        password = "password"
+        driver = self.driver
+        driver.implicitly_wait(20)
+        #my ip address
+        #driver.get("http://192.168.99.100:8000/")
+
+        #Travis
+        driver.get("http://174.138.62.246:8000/")
+        driver.find_element_by_id("id_username").send_keys(username)
+        driver.find_element_by_id("id_password").send_keys(password)
+        driver.find_element_by_id("login").click()
+        driver.implicitly_wait(20)
+        #Log out shit doesn't work right now.
+        #logout_text = driver.find_element_by_id("logout").text
+        #driver.implicitly_wait(20)
+        #self.assertEqual("Log Out", logout_text)
+
+    def test_user_signup(self):
+        username = "testuser"
+        email="test@virginia.edu"
+        password = "newpassword"
+        confirmpassword = "newpassword"
+        driver = self.driver
+        driver.implicitly_wait(20)
+        #my ip address
+        #driver.get("http://192.168.99.100:8000/signup/")
+
+        #travis
+        driver.get("http://174.138.62.246:8000/signup/")
+
+        driver.find_element_by_id("id_username").send_keys(username)
+        driver.find_element_by_id("id_email").send_keys(email)
+        driver.find_element_by_id("id_password").send_keys(password)
+        driver.find_element_by_id("id_passwordConfirm").send_keys(confirmpassword)
+        driver.find_element_by_id("signup").click()
+        driver.implicitly_wait(20)
+
+        #logout_text not working
+        #logout_text = driver.find_element_by_id("logout").text
+        #self.assertEqual("Log Out", logout_text)
+
+
+
+        #Just the test I made, but doesn't work -Taehyun-
+        #driver.find_element_by_xpath("/base/body/nav/div[2]/div[1]/div[1]/li/a").click()
+        #assert "Log Out:" in driver.page_source
+        #driver.implicitly_wait(20)
+
+
+    def test_home_page(self):
+        driver = self.driver
+        #My ip address
+        #driver.get("http://192.168.99.100:8000/home/")
+
+        #Travis
+        driver.get("http://174.138.62.246:8000/home/")
 
         #assert "ApartFinder is a website to help renters find tenants easily!" in driver.page_source
 
